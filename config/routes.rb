588182037bfc12c366bf15394a67846e3b2884dc
takumi-915 <59188083+takumi-915@users.noreply.github.com>
2020-01-06 12:34:10 +0900

    グループ機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
   root "messages#index"
 end
